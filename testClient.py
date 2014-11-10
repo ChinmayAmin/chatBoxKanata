5fe1c2008608da9dabae761b8c46d49ad1260d5f
ChinmayAmin <chinmayhamin@gmail.com>
2014-11-10 18:19:22 -0400

    works

--- testClient.py
+++ testClient.py
@@ -1,13 +1,27 @@
-import socket
+import socket,sys,select,SocketServer
 
 HOST = ''
-PORT = 9001
+PORT = 5001
 
 #According to docs the client sequence is
 #1)socket(),connect(),
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall('Hello, world')
-data = s.recv(1024)
-s.close()
-print 'Received', repr(data)
+
+while 1:
+	#since we need to listen to two things...1)user input 2)server sending messages
+	list = [sys.stdin,s]
+	
+	#same thing as server
+	read,write,error = select.select(list,[],[])
+	
+	for sk in read:
+		if sk == s: #server sending messages
+			data = sk.recv(4096)
+			sys.stdout.write(data)
+			sys.stdout.write('Enter something bro: ')
+			sys.stdout.flush()
+		else: #user entered something
+			s.send(sys.stdin.readline())
+			sys.stdout.write('Enter something bro: ')
+			sys.stdout.flush()
--- testServer.py
+++ testServer.py
@@ -8,33 +8,58 @@ import socket
 import SocketServer
 import select
 
-PORT = 9001
+PORT = 5001
 BUF_SIZE = 4096
 HOST = '' #Leaving it blank means it applies to all avaiable hardware interfaces...
-NUMBER_OF_CLIENTS = 1
+NUMBER_OF_CLIENTS = 5
 CONNECTED_CLIENTS = []
+ccount = 0;
 
+def send_to_all(sock,message):
+	#go through all of the client list
+	for sk in CONNECTED_CLIENTS:
+		if sk != main_socket and sk != sock:
+			sk.send(message)
 
 #From python documents..server goes through 4 steps
 #(1)socket(),bind(),listen(),accept()
 #Create the var to hold the socket stream
+
 main_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 main_socket.bind((HOST,PORT))
-main_socket.listen(NUMBER_OF_CLIENTS) #1 represents the number of clients it listens to
+main_socket.listen(NUMBER_OF_CLIENTS) #represents the number of clients it listens to
 
 #Create an arraylist to hold all of the connections
 CONNECTED_CLIENTS.append(main_socket)
 
-#Get a list of connections and check validity
-readable,writeable,error = select.select(CONNECTED_CLIENTS,[],[])
+print "started on port 5001 GG\n"
 
-#go through all of the readable connections
+while 1:
 
-for read in readable:
-    #Accept the client connection
-    connection,address = main_socket.accept()
-    print 'Connected dog to address:',address
-    while 1:
-        data = connection.recv(1024)
-        if not data: continue
-        connection.sendall(data)
+    #go through all of the readable connections
+    #print "Right before going into the read loop.."
+
+    readable,writeable,error = select.select(CONNECTED_CLIENTS,[],[])
+
+    #print "Right after select.select"
+
+    for read in readable:
+
+	if read == main_socket:
+		#when a client connects for the first time
+		#keep adding to connected_client list since more conncted
+		newClient,add = main_socket.accept()
+		CONNECTED_CLIENTS.append(newClient)
+		ccount = ccount + 1
+		print 'Client number' + str(ccount) + ' connected'
+	
+		#only need to send the welcome message to new clients..
+		newClient.send("Welcome to kanata chat box!\n")
+	elif read == sys.stdin:
+		#input from server side...for convinience
+		print "WHAT YOU DOING BRO\n"
+	else:
+		#data from client
+		data = read.recv(1024)
+		send_to_all(read,data)
+main_socket.close()
