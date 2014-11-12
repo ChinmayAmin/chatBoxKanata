3ab686cc417ba9a3d5554130110b014e5693e1e1
Chinmay Amin <chinmayhamin@gmail.com>
2014-11-12 00:12:14 -0500

    Added text box tehe

--- chatGui/testGui.py
+++ chatGui/testGui.py
@@ -1,1 +1,17 @@
 #will use Tkinter..just to try it
+#!/usr/bin/python
+
+from Tkinter import *
+
+top = Tk()
+top.width = 200
+top.height = 200
+
+#Creating simple chat box design...mind you i suck at graphics GG
+name_label = Label(top,text="Your name")
+name_label.pack( side = LEFT)
+text_input = Entry(top,bd = 5)
+
+text_input.pack(side = RIGHT)
+
+top.mainloop()
